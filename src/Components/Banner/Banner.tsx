8bfbbb58b2891d7fe8a11742cbd82af195b9763c
tAdithyan <Adithyanttech@gmail.com>
2024-08-02 20:07:43 +0530

    banner section make dynamic

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
-const Banner = () => {
+import { API_KEY } from '../../constants/constants';
+import axios from '../../axios';
+
+interface Movie {
+  title: string;
+  overview: string;
+  backdrop_path: string;
+}
+
+const Banner: React.FC = () => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(`/trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then(res => {
+        if (res.data.results && res.data.results.length > 0) {
+          const randomIndex = Math.floor(Math.random() * res.data.results.length);
+
+          setMovie(res.data.results[randomIndex]);
+          console.log(movie);
+
+        }
+      })
+      .catch(error => {
+        console.error('Failed to fetch movie data:', error);
+      });
+  }, []);
+
+  const backdropUrl = movie ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}` : '';
+
   return (
-    <div className="banner">
+    <div className="banner" style={{ backgroundImage: `url(${backdropUrl})` }}>
       <div className="content">
-        <h1 className="title">Movie NAme</h1>
-        <div className="banner_buttons">
-          <button className="button">play</button>
-          <button className="button">My-List</button>
-        </div>
-        <h3 className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ab nisi quibusdam natus
-          praesentium ducimus optio quis non, libero laborum! Fugit possimus sint earum velit
-          voluptates excepturi amet nam id.
-        </h3>
+        {movie ? (
+          <>
+            <h1 className="title">{movie.title}</h1>
+            <div className="banner_buttons">
+              <button className="button">Play</button>
+              <button className="button">My List</button>
+            </div>
+            <h3 className="description">{movie.overview}</h3>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
       <div className="fade_bottom"></div>
     </div>
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,3 +1,4 @@
-export const  base_url = 'https://api.themoviedb.org/3'
+export const base_url = 'https://api.themoviedb.org/3';
 
-export const API_KEY='aaf5e1fd5c30f0197e672da3d980f784'
+export const API_KEY = 'aaf5e1fd5c30f0197e672da3d980f784';
+export const IMG_URL = 'https://image.tmdb.org/t/p/original';
